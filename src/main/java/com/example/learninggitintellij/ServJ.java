d46df240f086dc2ed91377c5225021d7f1cc71bd
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-24 17:20:26 +0530

    Update ServJ.java
    SOP : Added World in >> Hai world I am ServJ

--- src/main/java/com/example/learninggitintellij/ServJ.java
+++ src/main/java/com/example/learninggitintellij/ServJ.java
@@ -2,6 +2,6 @@ package com.example.learninggitintellij;
 
 public class ServJ {
     public static void main(String[] args) {
-        System.out.println("Hai I am ServJ");
+        System.out.println("Hai world I am ServJ");
     }
 }
