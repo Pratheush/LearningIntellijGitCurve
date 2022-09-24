acd10e1e1784a5a42a3e58773bff6984239468a3
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-24 14:07:58 +0530

    Update DaoJ.java
    editing from MonuDevJP > PractisingIntellijGit > trial/trial1 : Edited Hello DaoJ

--- src/main/java/com/example/learninggitintellij/DaoJ.java
+++ src/main/java/com/example/learninggitintellij/DaoJ.java
@@ -2,6 +2,7 @@ package com.example.learninggitintellij;
 
 public class DaoJ {
     public static void main(String[] args) {
-        System.out.println("Hello Daoj");
+        System.out.println("Hello DaoJ");
+        // editing from MonuDevJP > PractisingIntellijGit > trial/trial1 : added Hello DaoJ
     }
 }
