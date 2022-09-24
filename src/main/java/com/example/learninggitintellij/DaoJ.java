eadddb8478b02b75e11e4394984c25b5de664e84
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-24 15:49:36 +0530

    Update DaoJ.java
    added some extra changes : SOP > Hello I am DaoJ

--- src/main/java/com/example/learninggitintellij/DaoJ.java
+++ src/main/java/com/example/learninggitintellij/DaoJ.java
@@ -2,7 +2,7 @@ package com.example.learninggitintellij;
 
 public class DaoJ {
     public static void main(String[] args) {
-        System.out.println("Hello DaoJ");
+        System.out.println("Hello I am DaoJ");
         // editing from MonuDevJP > PractisingIntellijGit > trial/trial1 : edited Hello DaoJ
     }
 }
