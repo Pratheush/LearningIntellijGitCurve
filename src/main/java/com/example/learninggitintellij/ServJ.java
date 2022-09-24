3bd8954db8c7cb76bdb86594156c1d21c4bb9451
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-24 17:11:34 +0530

    Update ServJ.java
    Changed SOP : Hai I am ServJ

--- src/main/java/com/example/learninggitintellij/ServJ.java
+++ src/main/java/com/example/learninggitintellij/ServJ.java
@@ -2,6 +2,6 @@ package com.example.learninggitintellij;
 
 public class ServJ {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hai I am ServJ");
     }
 }
