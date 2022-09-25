40557ad1c730ae6d6a58f229db39cd95fa8c379b
pratheush <pratheush@outlook.com>
2022-09-25 12:06:35 +0530

    Sixth Commit : my work DevJ :: SOP :: Pratheush Spring Application 1234

--- src/main/java/com/example/learninggitintellij/DevJ.java
+++ src/main/java/com/example/learninggitintellij/DevJ.java
@@ -1,9 +1,14 @@
 package com.example.learninggitintellij;
 
+import java.util.Arrays;
+
 public class DevJ {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
         // Pratheush > MonuDevJP ::: requesting add changes >> add SOP : springframework springboot
+
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Pratheush Spring Application 1234");
     }
 }
