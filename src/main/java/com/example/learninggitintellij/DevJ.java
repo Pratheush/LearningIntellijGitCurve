c351a725c27d2bad8d8787565f10f617506bd275
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-25 12:03:45 +0530

    Trial/trial1 (#6)
    * Update DaoJ.java
    
    added some extra changes : SOP > Hello I am DaoJ
    
    * Update ServJ.java
    
    Changed SOP : Hai I am ServJ
    
    * Update ServJ.java
    
    SOP : Added World in >> Hai world I am ServJ
    
    * Contribute and Add Changes : DevJ (#1)
    
    * Second Commit : Main I am ServJ Added
    
    * Third Commit : Main I am DaoJ Added
    
    * Second Commit : Main I am ServJ Added
    
    * Third Commit : Main I am DaoJ Added
    
    * Update DaoJ.java
    
    Updated : Hello Daoj
    
    * Update DaoJ.java (#3)
    
    added some extra changes : SOP > Hello I am DaoJ
    
    * Trial/trial1 (#4)
    
    * Update DaoJ.java
    
    added some extra changes : SOP > Hello I am DaoJ
    
    * Update ServJ.java
    
    Changed SOP : Hai I am ServJ
    
    * Update ServJ.java
    
    SOP : Added World in >> Hai world I am ServJ
    
    * Fourth Commit : added pom.xml
    
    * Fifth Commit : requesting add changes :: me working parallelly
    
    Co-authored-by: MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
    
    * Update DevJ.java
    
    added extra changes :: SOP  MonuDevJP spring application 5678
    
    Co-authored-by: Pratheush <pratheush@outlook.com>

--- src/main/java/com/example/learninggitintellij/DevJ.java
+++ src/main/java/com/example/learninggitintellij/DevJ.java
@@ -1,10 +1,11 @@
 package com.example.learninggitintellij;
 
-import java.util.Arrays;
-
 public class DevJ {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // Pratheush > MonuDevJP ::: requesting add changes >> add SOP : springframework springboot
+        System.out.println("MonuDevJP spring application 5678");
 
         // Pratheush > MonuDevJP ::: requesting add changes >> add SOP : springframework springboot
 
