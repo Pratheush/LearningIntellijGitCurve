1b59d55b352408e89c06fd7ea1a9d25be58327af
pratheush <pratheush@outlook.com>
2022-09-25 11:39:20 +0530

    Fifth Commit : requesting add changes :: me working parallelly

--- src/main/java/com/example/learninggitintellij/DevJ.java
+++ src/main/java/com/example/learninggitintellij/DevJ.java
@@ -3,5 +3,7 @@ package com.example.learninggitintellij;
 public class DevJ {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // Pratheush > MonuDevJP ::: requesting add changes >> add SOP : springframework springboot
     }
 }
